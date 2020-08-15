f13b8c04a746e5f7a039e0b7d8f1d5364dd6553e
FrankFuyanxiang <69617576+FrankFuyanxiang@users.noreply.github.com>
2020-08-15 13:11:01 +0800

    Add files via upload

---
+++ ipynb.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Cousera Capstone Project
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# ## Hello Capstone Project Course!
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
